9b857b7c4f03007eaddb28385986dca6b318798e
johntanderson <39395676+johntanderson@users.noreply.github.com>
2021-12-08 20:33:37 -0600

    Model Update

--- app.js
+++ app.js
@@ -6,35 +6,31 @@ const express = require('express'),
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req,res)=>{
+function runAsync (callback) {
+    return function (req, res, next) {
+        callback(req, res, next).catch(next)
+    }
+}
+
+app.get('/', (_,res)=>{
     res.render('landing');
 });
 
-app.get('/courses/:subject', (req,res)=>{
-    const subject = req.params.subject;
-    const title = `List of ${subject} Courses`;
-    if (!subjects.includes(subject)) return res.render('error');
-    CoursesModel.getList(subject, (fields, data)=>{
-        res.render('table', {
-            title: title,
-            fields: fields,
-            data: data
-        });
+app.get('/courses/:subject', runAsync(async (req, res) => {
+    if (!subjects.includes(req.params.subject)) return res.render('error');
+    res.render('table', {
+        title: `List of ${req.params.subject} Courses`,
+        data: await CoursesModel.getList(req.params.subject),
     });
-});
+}));
 
-app.get('/schedule/:subject', (req,res)=>{
-    const subject = req.params.subject;
-    const title = `Schedule of ${subject} Courses`;
-    if (!subjects.includes(subject)) return res.render('error');
-    CoursesModel.getSchedule(subject, (fields, data)=>{
-        res.render('table', {
-            title: title,
-            fields: fields,
-            data: data
-        });
+app.get('/schedule/:subject', runAsync(async (req, res) => {
+    if (!subjects.includes(req.params.subject)) return res.render('error');
+    res.render('table', {
+        title: `Schedule of ${req.params.subject} Courses`,
+        data: await CoursesModel.getSchedule(req.params.subject),
     });
-});
+}));
 
 app.listen(3000, ()=>{
     console.log("Server is running at localhost:3000...");
--- models/Courses.js
+++ models/Courses.js
@@ -1,99 +1,57 @@
 const { MongoClient } = require('mongodb');
 const url = "mongodb://localhost:27017/";
 const client = new MongoClient(url,  { useUnifiedTopology: true });
-Courses = {};
+const Courses = {};
+
+client.connect()
+.then( _ =>{
+    console.log("MongoDB connected!");
+    client.close();
+})
+.catch( err => {
+    console.log("An error occured while connecting to MongoDB!");
+})
 
 Courses.getList = async(subject) => {
+    let data = [["Subject", "#", 'Title', 'Description', 'Credits']]
     try {
         await client.connect();
-        let data = await client.db("uww").collection("courses").find({"subject": subject}).project({_id: 0, mincredits: 0, prereq: 0}).toArray();
-        client.close();
-        return data;
     } catch (err) {
         console.log("An error occured while connecting to DB.");
-        client.close();
-        return {};
+        return data;
     }
+    try {
+        let tmp = await client.db("uww").collection("courses").find({"subject": subject}).project({_id: 0, mincredits: 0, prereq: 0}).toArray();
+        tmp.forEach((record)=>{
+            data.push([record.subject, record.number, record.title, record.description, record.maxcredits])
+        });
+    } catch (err) {
+        console.log("An error occured while querying the DB.");
+    } finally {
+        client.close();
+        return data;
+    }
+
 }
 
 Courses.getSchedule = async(subject) => {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// client.connect(function(err) {
-//   if (err) throw err;
-//   console.log("done !");
-//   client.close();
-// });
-
-// client.connect((err) => {
-//   if (err) throw err;
-//       /*  Change the database. 
-//       */
-//       const dbo = client.db('uww');
-    
-//       /* Fetch data from 'courses' collection and then use .toArray() method to 
-//           to read all documents returned by the cursor
-//       */
-//       dbo.collection('courses').find({subject: "COMPSCI"}).toArray( (err, rows) => {
-//           if (err) throw err;
-//           const courseList = rows;
-//           console.log(courseList)
-//           if (courseList.length) {
-//               courseList.forEach(s => {
-//               });
-//           }
-//           client.close();
-//       });
-// });
-
-// conn.connect((err)=>{
-//     err ? console.log(err) : console.log("Connected to database.");
-// });
-
-// Courses.getList = (subject, callback)=> {
-    
-//     const query = "SELECT `subject`, `number`, `title`, `description`, `maxcredits` FROM `courses` WHERE `subject`= ? ORDER BY `number`";
-     
-//     conn.query(query, [subject], (err, results, fields)=>{
-//         if (err) throw err;
-//         fields = fields.map((field)=>{ return capitalizeFirstLetter(field.name);});
-//         callback(fields, results);
-//     })
-// }
-
-// Courses.getSchedule = (subject, callback)=> {
-//     const query = "SELECT `subject`, `number`, `sectionId`, `instructor`, `displayTime`, `location` FROM `schedule` WHERE `subject`= ? ORDER BY `number`, `sectionId`";
-//     conn.query(query, [subject], (err, results, fields)=>{
-//         if (err) throw err;
-//         fields = fields.map((field)=>{ return capitalizeFirstLetter(field.name);});
-//         callback(fields, results);
-//     })
-// }
-
-function capitalizeFirstLetter(string) {
-    if (string === "number") string = "#";
-    if (string === "maxcredits") string = "credits";
-    if (string === "displayTime") string = "time";
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    let data = [["Subject", "#", 'Section', 'Instructor', 'Time', 'Location']]
+    try {
+        await client.connect();
+    } catch (err) {
+        console.log("An error occured while connecting to DB.");
+        return data;
+    }
+    try {
+        let tmp = await client.db("uww").collection("schedule").find({"subject": subject}).project({_id: 0}).toArray();
+        tmp.forEach((record)=>{
+            data.push([record.subject, record.number, record.sectionId, record.instructor, record.displayTime, record.location])
+        });
+    } catch (err) {
+        console.log("An error occured while connecting to DB.");
+    }
+    client.close();
+    return data;
 }
 
 module.exports = Courses;
